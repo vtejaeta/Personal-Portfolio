8eb64f0abb2ad0be4f7daf83e6ce663bb9af1645
vtejaeta <vtejaeta@gmail.com>
2021-10-15 19:59:20 +0530

    Add social links

---
+++ src/components/SocialLinks/SocialLinks.js
@@ -0,0 +1,58 @@
+import React from "react"
+import styled from "styled-components"
+import Github from "../Icons/Github"
+import Linkedin from "../Icons/Linkedin"
+import Twitter from "../Icons/Twitter"
+
+const SocialIconsContainer = styled.div`
+  margin: 2rem;
+  display: flex;
+  justify-content: space-evenly;
+
+  @media only screen and (max-width: 767px) {
+    align-items: center;
+    justify-content: center;
+  }
+`
+const SocialIcon = styled.a`
+  padding: 1.1rem 1.3rem;
+  color: var(--invert-primary);
+
+  &:hover {
+    color: ${props => props.hoverColor};
+  }
+`
+
+export default function SocialLinks() {
+  return (
+    <SocialIconsContainer>
+      <SocialIcon
+        href="https://www.linkedin.com/in/viswa-teja-69b307189"
+        target="_blank"
+        rel="noopener noreferrer"
+        aria-label="Visit my Linkedin profile"
+        hoverColor="#0077b5"
+      >
+        <Linkedin />
+      </SocialIcon>
+      <SocialIcon
+        href="https://github.com/vtejaeta"
+        target="_blank"
+        rel="noopener noreferrer"
+        aria-label="Visit my Github profile"
+        hoverColor="grey"
+      >
+        <Github />
+      </SocialIcon>
+      <SocialIcon
+        href="https://twitter.com/vtejaeta9493"
+        target="_blank"
+        rel="noopener noreferrer"
+        aria-label="Visit my Twitter profile"
+        hoverColor="#1DA1F2"
+      >
+        <Twitter />
+      </SocialIcon>
+    </SocialIconsContainer>
+  )
+}
