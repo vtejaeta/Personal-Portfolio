e64fef1e9d5563fe39b115ab69c109dfa85ba6c1
vtejaeta <vtejaeta@gmail.com>
2021-10-15 19:55:40 +0530

    Add components for social icons

---
+++ src/components/Icons/Linkedin.js
@@ -0,0 +1,18 @@
+import React from "react"
+
+export default function Linkedin() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="23"
+      height="23"
+      viewBox="0 0 23 23"
+    >
+      <path
+        fill="currentColor"
+        fillRule="evenodd"
+        d="M0 0v24h24v-24h-24zm8 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.397-2.586 7-2.777 7 2.476v6.759z"
+      />
+    </svg>
+  )
+}
