7ac8f9dc8728d2dd4f0a1ced41fae6bd2e00de23
vtejaeta <vtejaeta@gmail.com>
2021-10-15 19:57:01 +0530

    Add utils for email

---
+++ src/utils/email.utils.js
@@ -0,0 +1,24 @@
+import emailjs from "emailjs-com"
+
+export function isEmailValid(email) {
+  const re =
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+  return re.test(String(email).toLowerCase())
+}
+
+export default async function sendEmail({ subject, username, email, message }) {
+  let response, errorMessage
+
+  try {
+    response = await emailjs.send(
+      "default_service",
+      "template_yxx45fm",
+      { subject, username, email, message },
+      "user_lAOwNk4jaXBkaKWAiWGur"
+    )
+  } catch (error) {
+    errorMessage = error
+  }
+
+  return { errorMessage, response }
+}
